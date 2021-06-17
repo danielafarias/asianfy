432f1bf57cb4fd14667ba83114d8edc0c78d71d5
daniela <farias.danicris@gmail.com>
2021-06-16 21:04:38 -0300

    Primeiro commit: Começo da estilização no SASS.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logo from './logo.png';
+import searchIcon from './img/searchIcon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='logoBar'>
+        <img className='logoImg' src={logo} alt='Asianfy logo'/>
+      </div>
+
+      <div className='searchBar'>
+        <img className='searchImg' src={searchIcon} alt='Asianfy logo'/>
+        <input className='searchBox' placeholder='Be curious!'/>
+      </div>
+    </>
   );
 }
 
